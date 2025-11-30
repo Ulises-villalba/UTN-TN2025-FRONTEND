cbb61fcc25e4096ba3bf9434f8ffc4f8b41f01b0
Ulises-Villalba <u.villalba2020@gmail.com>
2025-11-30 16:29:25 -0300

    commit fetch arreglados

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,52 +1,52 @@
-import { CONTENT_TYPE_VALUES, HEADERS, HTTP_METHODS } from "../constants/http"
+import { CONTENT_TYPE_VALUES, HEADERS, HTTP_METHODS } from "../constants/http";
 
-
+// Tomamos la URL del backend desde env de Vite
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function register(name, email, password) {
     const usuario = {
         email,
         username: name,
         password
-    }
+    };
 
-    //Queremos consumir nuesta API
-
-    //Ordena al navegador hacer una consulta HTTP
-    //recibe 2 parametros: la URL de consulta y un objeto de configuracion de consulta
     const response_http = await fetch(
-        'http://localhost:8080/api/auth/register',
+        `${API_URL}/api/auth/register`,
         {
             method: HTTP_METHODS.POST,
             headers: {
-                //Como vamos a enviar JSON, configuro que mi consulta envia contenido tipo JSON
                 [HEADERS.CONTENT_TYPE]: CONTENT_TYPE_VALUES.JSON
             },
             body: JSON.stringify(usuario)
         }
-    )
+    );
 
-    //Transformamos a objeto de JS el body de la respuesta
-    const response_data = await response_http.json()
-    if (!response_data.ok) {
-        throw new Error(response_data.message)
+    const response_data = await response_http.json();
+
+    if (!response_http.ok) {
+        throw new Error(response_data.message);
     }
-    return response_data
+
+    return response_data;
 }
 
 export async function login(email, password) {
-    const response = await fetch(
-        'http://localhost:8080/api/auth/login',
+    const response_http = await fetch(
+        `${API_URL}/api/auth/login`,
         {
             method: HTTP_METHODS.POST,
             headers: {
-                [HEADERS.CONTENT_TYPE]: CONTENT_TYPE_VALUES.JSON,
+                [HEADERS.CONTENT_TYPE]: CONTENT_TYPE_VALUES.JSON
             },
             body: JSON.stringify({ email, password })
-        })
-    const response_data = await response.json()
+        }
+    );
 
-    if (!response.ok) {
-        throw new Error(response_data.message)
+    const response_data = await response_http.json();
+
+    if (!response_http.ok) {
+        throw new Error(response_data.message);
     }
-    return response_data
+
+    return response_data;
 }
